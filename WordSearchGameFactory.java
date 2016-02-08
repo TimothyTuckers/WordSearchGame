93693b3239b111d38220180881cdab02652ac16c
Timothy Tucker <timothytuckers@gmail.com>
2016-02-08 11:32:22 -0600

    Create WordSearchGameFactory.java

---
+++ WordSearchGameFactory.java
@@ -0,0 +1,14 @@
+
+/**
+ * Provides a factory method for creating word search games. 
+ */
+public class WordSearchGameFactory {
+
+
+   public static WordSearchGame createGame() {
+   // change this to return an instance of your game engine
+      return new WordSearchGameEngine(); 
+   }
+   
+   
+}
